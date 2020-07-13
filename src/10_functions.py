15f011b0559de73d3449b1baf139fe338c2f6c88
Danica Valdez <danicaevaldez@gmail.com>
2020-07-12 19:51:21 -0700

    Functions completed

--- src/10_functions.py
+++ src/10_functions.py
@@ -8,5 +8,9 @@ num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
+def check_if_even(number):
+    if number % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd!")
 
